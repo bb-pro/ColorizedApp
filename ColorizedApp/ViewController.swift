229270da407e68feed519ba60ef7cb82e6e0e03b
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-06 15:11:05 +0500

    work with sliders

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    //MARK: - IB Outlets
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var sampleColor: UIView!
+    
+    //MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sampleColor.layer.cornerRadius = 30
+        updateColorValue()
+        updateColor()
+        
     }
-
-
+    
+    //MARK: - IB Actions
+    @IBAction func redSliderChanged() {
+        updateColorValue()
+        updateColor()
+    }
+    
+    @IBAction func greenSliderChanged() {
+        updateColorValue()
+        updateColor()
+    }
+    
+    @IBAction func blueSliderChanged() {
+        updateColorValue()
+        updateColor()
+    }
+    
+    //MARK: - Private Methods
+    private func updateColor() {
+        let colorResult = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        sampleColor.backgroundColor = colorResult
+    }
+    
+    private func updateColorValue() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
