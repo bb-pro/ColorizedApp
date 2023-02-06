4d76c2395a013c1f076b03c3f8c62ba62884b02a
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-06 10:07:14 +0500

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Bektemur on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
